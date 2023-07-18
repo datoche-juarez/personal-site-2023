9c45881ee1a8469cff24fe667931d96adbd5e349
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-07-18 01:04:16 -0400

    Testing git credentials again...

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center h-40 bg-dark-gray-300">
           <h1 className="text-xl">Home page placeholder...</h1>
         </div>
-        <div className="flex flex-col items-center justify-center h-40 bg-green-300">
+        <div className="flex flex-col items-center justify-center h-40 bg-gray-600">
           <h1>This is a second container...</h1>
         </div>
       </div>
