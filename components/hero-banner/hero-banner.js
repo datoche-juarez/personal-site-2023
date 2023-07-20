3705546d9077b8a54110ab9a085e0152322ffbb4
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2023-07-19 20:02:57 -0400

    Fixed footer height issues..

--- components/hero-banner/hero-banner.js
+++ components/hero-banner/hero-banner.js
@@ -3,6 +3,10 @@ import Image from "next/image";
 
 const HeroBanner = () => {
   return (
+    // <div className="h-500">
+    //   <div className="absolute inset-0 z-20 bg-orange-200 opacity-10"></div>
+    //   <Image className="relative z-10 min-w-full" src={Banner} alt="Banner" />
+    // </div>
     <div className="h-500">
       <div className="absolute inset-0 z-20 bg-orange-200 opacity-10"></div>
       <Image className="relative z-10 min-w-full" src={Banner} alt="Banner" />
