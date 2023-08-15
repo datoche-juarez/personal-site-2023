644502bb42e99fcdf9f20366e379ea980bbf88e4
Daniel Atoche-Juarez <dtoch123@gmail.com>
2023-08-15 19:01:01 -0400

    Hero banner work. Come back and fix banner position for mobile..

--- components/hero-banner/hero-banner.js
+++ components/hero-banner/hero-banner.js
@@ -2,14 +2,21 @@ import Banner from "../../assets/Banner.png";
 import Image from "next/image";
 
 const HeroBanner = () => {
+  const imageURL = "../../assets/Banner.png";
+
   return (
-    // <div className="h-500">
-    //   <div className="absolute inset-0 z-20 bg-orange-200 opacity-10"></div>
-    //   <Image className="relative z-10 min-w-full" src={Banner} alt="Banner" />
-    // </div>
-    <div className="mt-0 h-500">
-      <div className="absolute inset-0 z-20 bg-orange-500 opacity-10"></div>
-      <Image className="relative z-10 min-w-full" src={Banner} alt="Banner" />
+    <div className="relative z-10 bg-cover h-500">
+      <div className="absolute inset-0 z-30 bg-orange-500 opacity-10"></div>
+      <Image
+        className="absolute inset-0 z-20 object-cover w-full h-full"
+        src={Banner}
+        alt="Banner"
+      />
+      <div className="absolute bottom-0 right-0 z-40 flex justify-center px-16 pb-10 m-auto">
+        <p className="font-semibold text-white text-7xl font-dmMono">
+          Daniel Atoche-Juarez
+        </p>
+      </div>
     </div>
   );
 };
