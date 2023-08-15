53a5379243dca39df5ca6494167750eccc1c6652
Daniel Atoche-Juarez <dtoch123@gmail.com>
2023-08-15 19:43:08 -0400

    Updated new home page welcome message..

--- app/page.js
+++ app/page.js
@@ -2,8 +2,14 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col border-2 border-red-400">
-        <div className="flex flex-col items-center justify-center h-40 bg-dark-gray-300">
-          <h1 className="text-xl">Home page placeholder...</h1>
+        <div className="flex flex-col items-center justify-center h-48 p-20">
+          <h1 className="text-xl">
+            Welcome to my corner of the web! I&rsquo;m a recent college graduate with
+            a passion for creating innovative software solutions. With dual
+            degrees in Computer Information Systems and Liberal Arts, I bring a
+            unique blend of technical expertise and creative thinking to every
+            project.
+          </h1>
         </div>
         <div className="flex flex-col items-center justify-center h-40 bg-gray-600">
           <h1>This is a second container...</h1>
