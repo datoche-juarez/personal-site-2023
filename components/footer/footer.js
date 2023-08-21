68329a0fe00d848282c38c5a2bb52a9b531e5748
Daniel Atoche-Juarez <dtoch123@gmail.com>
2023-08-21 19:10:06 -0400

    Fixed margin issues in footer...

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,10 +5,10 @@ import SocialContainer from "../social-container/social-container";
 const Footer = () => {
   return (
     <div className="flex flex-col-reverse max-w-full px-4 py-10 mt-20 h-200 bg-dark-gray md:flex-row">
-      <div className="self-center w-full p-2">
-        <div className="flex flex-col items-center p-2 md:items-start">
+      <div className="self-center w-full p-2 md:pl-0">
+        <div className="flex flex-col items-center p-2 md:items-start md:pl-0">
           <ul className="flex flex-col justify-center text-center xs:flex-row xs:justify-start xs:text-left">
-            <li className="px-1 text-sm font-light text-white hover:text-dark-gray-300 sm:text-base sm:px-2">
+            <li className="pl-2 pr-2 text-sm font-light text-white hover:text-dark-gray-300 sm:text-base md:pr-2 md:pl-0">
               <Link href="/">HOME</Link>
             </li>
             <li className="px-1 text-sm font-light text-white hover:text-dark-gray-300 sm:px-2 sm:text-base">
@@ -22,13 +22,13 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="flex flex-col items-center justify-center h-auto min-h-full p-2 md:items-start">
+        <div className="flex flex-col items-center justify-center h-auto min-h-full py-2 md:items-start">
           <p className="text-sm font-light text-center text-dark-gray-500 xs:text-left">
             © Daniel Atoche-Juarez {dayjs().year()}
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center self-center justify-center w-full p-4 max-h-fit md:items-end">
+      <div className="flex flex-col items-center self-center justify-center w-full py-4 max-h-fit md:items-end">
         <SocialContainer />
       </div>
     </div>
