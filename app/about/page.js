e488532d6016c6bd1f4c7c9c9f035cc2658a00ea
Daniel Atoche-Juarez <dtoch123@gmail.com>
2023-08-31 00:29:59 -0400

    Adding missing title for about page section..

--- app/about/page.js
+++ app/about/page.js
@@ -4,10 +4,10 @@ export default function About() {
   return (
     <>
       <div className="px-4 bg-red-200 border-2 border-red-500">
-        <div className="flex flex-col items-center justify-center border-2 border-green-400 ">
-          {/* <p className="mb-2 text-2xl font-bold font-inter md:pt-10 lg:text-2xl xl:text-2xl">
+        <div className="flex flex-col items-center justify-center text-left border-2 border-green-400 ">
+          <p className="w-full mb-2 ml-4 text-2xl font-bold text-left font-inter md:pt-10 lg:text-2xl xl:text-2xl">
             About Me
-          </p> */}
+          </p>
           <h1 className="m-4 text-xl">
             I thrive on turning ideas into reality through coding. With a solid
             foundation in Computer Information Systems and a creative touch from
