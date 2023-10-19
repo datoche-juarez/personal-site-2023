eaca3f52704876eb6e19d79421ad49124ce110ec
Daniel Atoche-Juarez <dtoch123@gmail.com>
2023-10-19 16:15:21 -0400

    Testing changes..

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <div className="px-4 bg-red-200 border-2 border-red-500">
+      <div className="px-4 bg-red-200 border-2 border-blue-500">
         <div className="flex flex-col items-center justify-center text-left border-2 border-green-400 ">
           <p className="w-full mb-2 ml-4 text-2xl font-bold text-left font-inter md:pt-10 lg:text-2xl xl:text-2xl">
             About Me
